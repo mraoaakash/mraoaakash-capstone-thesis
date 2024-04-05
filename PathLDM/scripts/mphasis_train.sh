b02bebc52551a66482724fc4b4951b1a95728e3a
Aakash Rao <mrao.aakash@gmail.com>
2024-04-05 10:41:49 +0530

    updated environment variable

--- PathLDM/scripts/mphasis_train.sh
+++ PathLDM/scripts/mphasis_train.sh
@@ -2,5 +2,9 @@
 eval "$(conda shell.bash hook)"
 conda activate pathldm
 
+BASEPATH=/home/aakashrao/research/research-thesis/mraoaakash-capstone-thesis/PathLDM
+
+cd $BASEPATH
+
 wandb disabled
-python /home/aakashrao/research/research-thesis/mraoaakash-capstone-thesis/PathLDM/main.py -t --gpu 1  --base scripts/configs/clip_imagenet_finetune.yaml --name testing --train True --logdir /mnt/storage/aakashrao/cifsShare/PathLDM/outputs
+python main.py -t --gpu 1  --base scripts/configs/clip_imagenet_finetune.yaml --name testing --train True --logdir /mnt/storage/aakashrao/cifsShare/PathLDM/outputs
