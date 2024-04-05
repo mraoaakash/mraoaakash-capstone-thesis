442e5c58884499adb792c125366e74e93b6c24cd
Aakash Rao <mrao.aakash@gmail.com>
2024-04-05 10:40:41 +0530

    updated environment variable

--- PathLDM/scripts/mphasis_train.sh
+++ PathLDM/scripts/mphasis_train.sh
@@ -7,4 +7,4 @@ BASEPATH=/mnt/storage/aakashrao/cifsShare/PathLDM
 cd $BASEPATH
 
 wandb disabled
-python main.py -t --gpu 1  --base scripts/configs/clip_imagenet_finetune.yaml --name testing --train True --logdir /mnt/storage/aakashrao/cifsShare/PathLDM/outputs
+python ../main.py -t --gpu 1  --base scripts/configs/clip_imagenet_finetune.yaml --name testing --train True --logdir /mnt/storage/aakashrao/cifsShare/PathLDM/outputs
