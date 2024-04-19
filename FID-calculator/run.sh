a57be3f1b0ec55542d8cad4e31d0f6b43a5523b2
Aakash Rao <mrao.aakash@gmail.com>
2024-04-19 11:55:31 +0530

    multiple changes

--- FID-calculator/run.sh
+++ FID-calculator/run.sh
@@ -9,7 +9,7 @@ do
     python scripts/extract_from_test.py \
         --data_dir $BASEPATH \
         --token_num  $i   \
-        --outdir $BASEPATH/original_images
+        --outdir $BASEPATH/TCGA_Dataset_images/original_images
 
     # python scripts/generate_images.py \
     #     --ckpt_path $SHAREPATH/PathLDM/outputs/04-08T22-44_testing/checkpoints/last.ckpt \
