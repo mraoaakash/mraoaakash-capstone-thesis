e76aadf213828dd23fe30aadb44704193e9e53e7
Aakash Rao <mrao.aakash@gmail.com>
2024-04-24 16:52:30 +0530

    added a bunch of stuff

--- GPT-API/scripts/evaluate_summaries.py
+++ GPT-API/scripts/evaluate_summaries.py
@@ -24,3 +24,11 @@ if __name__ =="__main__":
     argparser.add_argument("--token_num", type=str, required=True)
     args = argparser.parse_args()
 
+    main_summary_path = os.path.join(args.input, "all_cleaned_summaries.csv")
+    other_summary_path = os.path.join(args.input, f"summaries_{args.token_num}", f"summaries_{args.token_num}.json")
+
+    main_summaries = pd.read_csv(main_summary_path)
+    
+    other_summaries = json.load(open(other_summary_path, "r"))
+    other_summaries = pd.DataFrame(other_summaries)
+
