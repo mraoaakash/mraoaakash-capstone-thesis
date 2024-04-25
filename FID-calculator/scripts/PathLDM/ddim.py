2c3905ab6fa0f1b288cff53b3214d8f65bd9e844
Aakash Rao <mrao.aakash@gmail.com>
2024-04-25 09:57:40 +0530

    added info

--- FID-calculator/scripts/PathLDM/ddim.py
+++ FID-calculator/scripts/PathLDM/ddim.py
@@ -5,7 +5,7 @@ import numpy as np
 from tqdm import tqdm
 from functools import partial
 
-from diffusion_utils.util import make_ddim_sampling_parameters, make_ddim_timesteps, noise_like
+from diffusion_utils import make_ddim_sampling_parameters, make_ddim_timesteps, noise_like
 
 
 class DDIMSampler(object):
