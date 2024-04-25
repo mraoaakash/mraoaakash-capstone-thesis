ce35e3e2ac60d0df2f89c37ee8c9b444eabe6b36
Aakash Rao <mrao.aakash@gmail.com>
2024-04-25 11:43:47 +0530

    updated permissions

--- FID-calculator/run.sh
+++ FID-calculator/run.sh
@@ -3,7 +3,7 @@ BASEPATH=$SHAREPATH/PathLDM/inputs
 
 
 # for loop for vales 20,35,50
-for i in 75 20 #35 50
+for i in 75 #20 #35 50
 do
     # echo "Processing for token length $i"
     # python scripts/extract_from_test.py \
