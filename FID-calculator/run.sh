993ade1e2f6244541217485cb6469a60072353b6
Aakash Rao <mrao.aakash@gmail.com>
2024-04-25 09:04:25 +0530

    added info

--- FID-calculator/run.sh
+++ FID-calculator/run.sh
@@ -11,10 +11,12 @@ do
     #     --token_num  $i   \
     #     --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images
 
+    echo "/media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i"
+    
     python scripts/generate_images.py \
         --ckpt_path $SHAREPATH/PathLDM/outputs/04-08T22-44_testing/checkpoints/last.ckpt \
         --config_path $SHAREPATH/PathLDM/outputs/04-08T22-44_testing/configs/04-08T22-44-project.yaml \
         --data_dir $BASEPATH \
         --token_num $i \
-        --outdir $BASEPATH/generated_$i 
+        --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i
 done
