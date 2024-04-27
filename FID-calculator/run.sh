a6a279525aba268674b25c92b05cbe3119d01df8
Aakash Rao <mrao.aakash@gmail.com>
2024-04-27 13:32:22 +0530

    changed tokens

--- FID-calculator/run.sh
+++ FID-calculator/run.sh
@@ -30,7 +30,4 @@ echo "/media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i"
 # python -m pytorch_fid --save-stats /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_feed/images
 
 
-python scripts/fid.py \
-    --real_path /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images/images.npz \
-    --generated /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_75 \
-    --batch_size 32
+python -m pytorch_fid /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images/images.npz /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_75 
