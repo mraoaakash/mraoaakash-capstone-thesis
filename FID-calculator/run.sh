c050d8e25eb746b1e1531682294cd5710b4f65ce
Aakash Rao <mrao.aakash@gmail.com>
2024-04-27 12:55:32 +0530

    changed tokens

--- FID-calculator/run.sh
+++ FID-calculator/run.sh
@@ -1,23 +1,27 @@
 SHAREPATH=/mnt/storage/aakashrao/cifsShare
 BASEPATH=$SHAREPATH/PathLDM/inputs
 
-
 # for loop for vales 20,35,50
-for i in 75 #20 #35 50
-do
-    # echo "Processing for token length $i"
-    # python scripts/extract_from_test.py \
-    #     --data_dir $BASEPATH \
-    #     --token_num  $i   \
-    #     --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images
+# echo "Processing for token length $i"
+# python scripts/extract_from_test.py \
+#     --data_dir $BASEPATH \
+#     --token_num  $i   \
+#     --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images
 
-    echo "/media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i"
+echo "/media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i"
 
-    python scripts/generate_images.py \
-        --ckpt_path $SHAREPATH/PathLDM/outputs/04-08T22-44_testing/checkpoints/last.ckpt \
-        --config_path $SHAREPATH/PathLDM/outputs/04-08T22-44_testing/configs/04-08T22-44-project.yaml \
-        --data_dir $BASEPATH \
-        --token_num $i \
-        --batch_size 32 \
-        --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i
-done
+# python scripts/generate_images.py \
+#     --ckpt_path $SHAREPATH/PathLDM/outputs/04-08T22-44_testing/checkpoints/last.ckpt \
+#     --config_path $SHAREPATH/PathLDM/outputs/04-08T22-44_testing/configs/04-08T22-44-project.yaml \
+#     --data_dir $BASEPATH \
+#     --token_num 75 \
+#     --batch_size 32 \
+#     --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i
+
+python scripts/generate_images.py \
+    --ckpt_path $SHAREPATH/PathLDM/outputs/04-20T12-35_testing/checkpoints/last.ckpt \
+    --config_path $SHAREPATH/PathLDM/outputs/04-20T12-35_testing/configs/04-20T12-35-project.yaml \
+    --data_dir $BASEPATH \
+    --token_num 20 \
+    --batch_size 32 \
+    --outdir /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i
