9c2a0f7783c74abb67153dd5f185c5ace9ec14e5
Aakash Rao <mrao.aakash@gmail.com>
2024-04-29 09:57:40 +0530

    added FID script line for token_len=20

--- FID-calculator/run.sh
+++ FID-calculator/run.sh
@@ -33,4 +33,4 @@ echo "/media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_$i"
 # python -m pytorch_fid /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_75/images --batch-size 32
 
 
-python -m pytorch_fid /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_20/images --batch-size 32
+python -m pytorch_fid /media/chs.gpu/DATA/aakash-work/PathLDM/input/original_images /media/chs.gpu/DATA/aakash-work/PathLDM/input/generated_20/images --batch-size 128
