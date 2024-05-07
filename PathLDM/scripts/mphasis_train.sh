3ff2d8e1a2de1528076ab11563ceb6f3dee1f5b0
Aakash Rao <mrao.aakash@gmail.com>
2024-05-07 10:48:26 +0530

    updated

--- PathLDM/scripts/mphasis_train.sh
+++ PathLDM/scripts/mphasis_train.sh
@@ -7,5 +7,6 @@ BASEPATH=/home/aakashrao/research/research-thesis/mraoaakash-capstone-thesis/Pat
 cd $BASEPATH
 
 wandb disabled
-python main.py -t --gpus 1  --base scripts/configs/154_MPHASIS_clip_imagenet_finetune.yaml --name testing --train True --logdir /mnt/storage/aakashrao/cifsShare/PathLDM/outputs/test_memory
 # python main.py -t --gpus 1  --base scripts/configs/MPHASIS_clip_imagenet_finetune.yaml --name testing --train True --logdir /mnt/storage/aakashrao/cifsShare/PathLDM/outputs
+
+python main.py -t --gpus 1  --base scripts/configs/MPHASIS_clip_imagenet_finetune.yaml --name testing --train True --logdir /mnt/storage/aakashrao/cifsShare/PathLDM/outputs/test_memory
